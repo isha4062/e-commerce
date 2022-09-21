ba74e7bd3961cfb3fab234ed884ead7e68044c2d
Isha <isha.20hcs4125@ddu.du.ac.in>
2022-09-22 03:40:36 +0530

    filters added

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,7 +1,7 @@
 /* eslint-disable array-callback-return */
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Checkbox, Col, Grid } from "rsuite";
+import { Col, Grid } from "rsuite";
 import Products from "../products.json";
 import CartItem from "./CartItem";
 import Product from "./Product";
@@ -9,7 +9,7 @@ import Product from "./Product";
 function Category() {
   const { pid } = useParams();
   const [item, setItem] = useState([]);
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState("all");
   useEffect(() => {
     const data = [];
     Products.map((product) => {
@@ -20,9 +20,9 @@ function Category() {
     setItem(data);
   }, [pid]);
 
-  const onChecked = useCallback(() => {
-    setChecked((p) => !p);
-  }, []);
+  const handleChange = (e) => {
+    setChecked(e.target.value);
+  };
 
   return (
     <>
@@ -30,14 +30,39 @@ function Category() {
         <Col xs={24} md={6}>
           <div className="filter">
             <h1>Filters</h1>
-            <Checkbox onClick={onChecked} value={checked}>
-              Delivery
-            </Checkbox>
+            <div className="radio">
+              <label htmlFor="all">All</label>
+              <input type="radio" name="fiter" value="all" id="all" onChange={handleChange} />
+            </div>
+            <div className="radio">
+              <label htmlFor="delivery">Delivery</label>
+              <input type="radio" name="fiter" value="delivery" id="delivery" onChange={handleChange} />
+            </div>
+            <h4>Price</h4>
+            <div className="radio">
+              <label htmlFor="80">Below 80 USD</label>
+              <input type="radio" name="fiter" value="80" id="80" onChange={handleChange} />
+            </div>
+            <div className="radio">
+              <label htmlFor="80-100">Between 80-100 USD</label>
+              <input type="radio" name="fiter" value="80-100" id="80-100" onChange={handleChange} />
+            </div>
+            <div className="radio">
+              <label htmlFor="100">Above 100 USD</label>
+              <input type="radio" name="fiter" value="100" id="100" onChange={handleChange} />
+            </div>
           </div>
         </Col>
         <Col xs={24} md={18}>
           <CartItem />
-          {checked && (
+          {checked === "all" && (
+            <div className="products">
+              {item.map((ele) => (
+                <Product prod={ele} key={ele.id} />
+              ))}
+            </div>
+          )}
+          {checked === "delivery" && (
             <div className="products">
               {item.map((ele) => {
                 if (ele.delivery) {
@@ -46,11 +71,31 @@ function Category() {
               })}
             </div>
           )}
-          {!checked && (
+          {checked === "80" && (
             <div className="products">
-              {item.map((ele) => (
-                <Product prod={ele} key={ele.id} />
-              ))}
+              {item.map((ele) => {
+                if (ele.price < 80) {
+                  return <Product prod={ele} key={ele.id} />;
+                }
+              })}
+            </div>
+          )}
+          {checked === "80-100" && (
+            <div className="products">
+              {item.map((ele) => {
+                if (ele.price >= 80 && ele.price <= 100) {
+                  return <Product prod={ele} key={ele.id} />;
+                }
+              })}
+            </div>
+          )}
+          {checked === "100" && (
+            <div className="products">
+              {item.map((ele) => {
+                if (ele.price > 100) {
+                  return <Product prod={ele} key={ele.id} />;
+                }
+              })}
             </div>
           )}
         </Col>
